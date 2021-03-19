7b6f0396c91f3e54f0f93bab56231235833c4a76
zviels <59054323+zviels@users.noreply.github.com>
2021-03-19 16:13:47 +0200

    Added Search Functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 // Elements
 
 const gallery = document.querySelector('#gallery');
+const searchBox = document.querySelector('.search-container');
+
+// Getters
+
+const getSearchButton = () => document.querySelector('#search-submit');
+const getSearchInput = () => document.querySelector('#search-input');
+const getCardNames = () => document.querySelectorAll('.card-name');
 
 // Auxiliary Functions
+
+// createSearchBar
+
+const createSearchBar = () => {
+
+    const html = `<form action="#" method="get">
+                    <input type="search" id="search-input" class="search-input" placeholder="Search ...">
+                    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                  </form>`;  
+
+    searchBox.insertAdjacentHTML('beforeend', html);
+
+}
+
+// createGallery
 
 const createGallery = (users) => {
 
@@ -21,6 +43,38 @@ const createGallery = (users) => {
         gallery.insertAdjacentHTML('beforeend', html);
 
     });
+    
+}
+
+// filterUsers
+
+const filterUsers = (value) => {
+    
+    getCardNames().forEach((cardName) => {
+
+        const card = cardName.parentNode.parentNode;
+
+        if (!(cardName.textContent.includes(value)))
+            card.style.display = 'none';
+        
+        else
+            card.style.display = '';    
+
+    });
+
+}
+
+// Event Listeners
+
+const addListeners = () => {
+
+    const addButtonListener = () => getSearchButton().addEventListener('click', () => filterUsers(getSearchInput().value));
+    const addInputListener = () => getSearchInput().addEventListener('input', (e) => filterUsers(e.target.value));
+    
+    // Actually Add The Event Listeners
+
+    addButtonListener();
+    addInputListener();
 
 }
 
@@ -29,7 +83,19 @@ const createGallery = (users) => {
 const fetchUsers = (url) => fetch(url)
                     .then((res) => res.json());
 
+// Activation Functions
+
+const run = () => {
+
+    createSearchBar();
+
+    fetchUsers('https://randomuser.me/api/?results=12&nat=us')
+        .then((users) => createGallery(users.results));
+
+    addListeners();    
+
+}
+
 // Run The Script
 
-fetchUsers('https://randomuser.me/api/?results=12&nat=us')
-    .then((users) => createGallery(users.results));
+run();
